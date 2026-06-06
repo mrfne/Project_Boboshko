0f9ab598bbb28fc4bb5bac7997bc7b41ff75bd22
mrfne <thedaspi000@gmail.com>
2026-06-06 14:03:49 +0300

    PZ_11_1.py

--- PZ_11_1.py
+++ PZ_11_1.py
@@ -2,7 +2,6 @@
 # исходной последовательности организовать первую последовательность, содержащую
 # числа кратные трем, и вторую – для всех остальных. Найти количество элементов в
 # полученных последовательностях.
-# 2. Составить генератор (yield), который выводит из строки только цифры
 
 import random
 # 1
@@ -12,12 +11,3 @@ triples = [x for x in numbers if x % 3 == 0]
 others = [x for x in numbers if x % 3 != 0]
 print(triples, len(triples))
 print(others, len(others))
-
-# 2
-def digits(s):
-    for ch in s:
-        if ch.isdigit():
-            yield ch
-
-s = "апельсины 45 991 63 100 12 яблоки 13 47 26 0 16"
-print(list(digits(s)))
