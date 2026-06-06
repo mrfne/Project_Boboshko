538905d979ab97395db44650a297c89ebba5d469
mrfne <thedaspi000@gmail.com>
2026-06-06 13:46:17 +0300

    PZ_8_1.py

---
+++ PZ_8_1.py
@@ -0,0 +1,6 @@
+# Задание 1: Создать словарь из списков keys = ['Ten', 'Twenty', 'Thirty'] и values = [10, 20, 30]
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+dictionary = dict(zip(keys, values))
+print("Словарь:", dictionary)
